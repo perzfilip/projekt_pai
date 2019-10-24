2d9b9790ebb7c4fbd7ecd3bd684fd0ccc41d8686
Robsonwp <perzfilip@gmail.com>
2019-10-24 20:52:33 +0200

    robota nad nauka idą do przodu

--- front/js/script.js
+++ front/js/script.js
@@ -2,13 +2,10 @@ function navbarDropdown() {
     document.getElementById("dropdown").classList.toggle("show");
 }
 
-window.addEventListener('load', function () {
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        }
-    })
-});
+function clicked(elem) {
+   if(elem.classList.contains('clicked'))
+        elem.classList.remove('clicked');
+   else
+       elem.classList.add('clicked');
+}
+
