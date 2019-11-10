6f3dde87f714450f52dda537eadfc26e4ca31ec9
Robsonwp <perzfilip@gmail.com>
2019-11-10 16:47:35 +0100

    zarządzanie fiszkami na glownej + baza danych do poprawki

---
+++ database/Filip_Perz_PAI_mysql_create.sql
@@ -0,0 +1,74 @@
+CREATE TABLE `Users` (
+	`user_id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`login` varchar(30) NOT NULL UNIQUE,
+	`password` varchar(255) NOT NULL,
+	`firstName` varchar(30) NOT NULL,
+	`lastName` varchar(30) NOT NULL,
+	`status` INT(30) NOT NULL,
+	`role` INT NOT NULL,
+	`class` INT,
+	PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `Status` (
+	`status_id` INT NOT NULL AUTO_INCREMENT,
+	`statusName` varchar(40) NOT NULL,
+	PRIMARY KEY (`status_id`)
+);
+
+CREATE TABLE `Roles` (
+	`role_id` INT NOT NULL AUTO_INCREMENT,
+	`roleName` varchar(30) NOT NULL,
+	PRIMARY KEY (`role_id`)
+);
+
+CREATE TABLE `Classes` (
+	`class_id` INT NOT NULL AUTO_INCREMENT,
+	`className` varchar(10) NOT NULL,
+	PRIMARY KEY (`class_id`)
+);
+
+CREATE TABLE `Sets` (
+	`set_id` INT NOT NULL AUTO_INCREMENT,
+	`setName` varchar(50) NOT NULL,
+	`language` INT(50) NOT NULL,
+	`createdBy` INT NOT NULL,
+	PRIMARY KEY (`set_id`)
+);
+
+CREATE TABLE `Languages` (
+	`language_id` INT NOT NULL AUTO_INCREMENT,
+	`languageName` varchar(30) NOT NULL,
+	PRIMARY KEY (`language_id`)
+);
+
+CREATE TABLE `Flashcards` (
+	`flashcard_id` INT NOT NULL AUTO_INCREMENT,
+	`word` varchar(100) NOT NULL,
+	`definition` varchar(100) NOT NULL,
+	`Set` INT NOT NULL,
+	PRIMARY KEY (`flashcard_id`)
+);
+
+CREATE TABLE `Sets_to_classes` (
+	`class` INT NOT NULL,
+	`set` INT NOT NULL,
+	PRIMARY KEY (`class`,`set`)
+);
+
+ALTER TABLE `Users` ADD CONSTRAINT `Users_fk0` FOREIGN KEY (`status`) REFERENCES `Status`(`status_id`);
+
+ALTER TABLE `Users` ADD CONSTRAINT `Users_fk1` FOREIGN KEY (`role`) REFERENCES `Roles`(`role_id`);
+
+ALTER TABLE `Users` ADD CONSTRAINT `Users_fk2` FOREIGN KEY (`class`) REFERENCES `Classes`(`class_id`);
+
+ALTER TABLE `Sets` ADD CONSTRAINT `Sets_fk0` FOREIGN KEY (`language`) REFERENCES `Languages`(`language_id`);
+
+ALTER TABLE `Sets` ADD CONSTRAINT `Sets_fk1` FOREIGN KEY (`createdBy`) REFERENCES `Users`(`user_id`);
+
+ALTER TABLE `Flashcards` ADD CONSTRAINT `Flashcards_fk0` FOREIGN KEY (`Set`) REFERENCES `Sets`(`set_id`);
+
+ALTER TABLE `Sets_to_classes` ADD CONSTRAINT `Sets_to_classes_fk0` FOREIGN KEY (`class`) REFERENCES `Classes`(`class_id`);
+
+ALTER TABLE `Sets_to_classes` ADD CONSTRAINT `Sets_to_classes_fk1` FOREIGN KEY (`set`) REFERENCES `Sets`(`set_id`);
+
