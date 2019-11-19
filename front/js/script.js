0d18a49ce6810583ecce603b6eb1c26d533b0960
Robsonwp <perzfilip@gmail.com>
2019-11-19 22:47:06 +0100

    dodawanie kolejnych fiszek + przygotowanie do usuwania

--- front/js/script.js
+++ front/js/script.js
@@ -24,3 +24,18 @@ function login_deanimate() {
 function wrap(elem) {
     elem.nextElementSibling.classList.toggle("unwrap");
 }
+
+function menageFlashcard(a) {
+    let flashcards = document.querySelectorAll("div.one_flashcard");
+    let parent = flashcards[0].parentNode;
+    a = parseInt(a);
+    if(a === -1) {
+        let div = document.createElement("div");
+        div.classList.add("one_flashcard");
+        div.innerHTML =  '<div class="input"><div><input type="text" name="title" placeholder=" " autocomplete="off" required><span>Słówko</span></div></div><div class="input"><div><input type="text" name="title" placeholder=" " autocomplete="off" required><span>Definicja</span></div></div>';
+        flashcards[0].parentNode.append(div);
+    } else {
+        //todo usuwanie do zaimplementowania
+        parent.removeChild(flashcards[a]);
+    }
+}
