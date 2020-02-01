79c082688d7788a94e3b0293b2fa91932f0dcdb5
Filip Perz <perzfilip@gmail.com>
2020-02-01 20:25:36 +0100

    test

--- final/password_generator.php
+++ final/password_generator.php
@@ -4,4 +4,4 @@
     $hashPassword = password_hash($password, PASSWORD_ARGON2ID);
 
     echo "hasło: ", $password, "<br>";
-    echo "zahashowane hasło: ", $hashPassword, "<br>";
+    echo "zahdashowane hasło: ", $hashPassword, "<br>";
